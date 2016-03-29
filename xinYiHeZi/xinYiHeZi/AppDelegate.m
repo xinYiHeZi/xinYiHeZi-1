6a39637726e3478a7978003320c6f54d00ef8937
miss_tan <1054253786@qq.com>
2016-03-29 21:39:53 +0800

    tanzhiwen

--- xinYiHeZi/xinYiHeZi/AppDelegate.m
+++ xinYiHeZi/xinYiHeZi/AppDelegate.m
@@ -38,6 +38,8 @@
     NSLog(@"/**************^-^*********************/");
 NSLog(@"%s", __FUNCTION__);
 NSLog(@"%s", __FUNCTION__);
+    NSLog(@"%s", __FUNCTION__);
+    
     /**
      *  lallalallaala
      */
